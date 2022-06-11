dca04c2592c835f3ebafacd9bbed115cb87d652f
Sule Aishat Aladi <suleaishat990@gmail.com>
2022-06-11 09:30:58 +0100

    initial_commit

---
+++ fa_classifier.py
@@ -0,0 +1,26 @@
+from cProfile import label
+from gravityai import gravityai as grav
+import pickle
+import pandas as pd
+
+
+model = pickle.load(open(''))
+    #Term Frequency - Inverse Document Frequency (TFIDF)
+
+tfidf_vectorizer = pickle.load(open(''))
+label_encoder = pickle.load(open(''))
+
+def process(inPath, outPath):
+    #read input file inpath
+    input_df = pd.read_csv(inPath)
+    #vectorise the data to figure out which words weigh heavily in our data or articles
+    features = tfidf_vectorizer.transform(input_df['body'])
+    #predict the classes
+    predictions = model.predict(features)
+    #convert output labels to categories
+    input_df['category'] = label_encoder.inverse_transform(predictions)
+    #save results to csv
+    output_df = input_df[['id', 'category']]
+    output_df.to_csv(outPath, index=False)
+
+grav.wait_for_requests(process)
